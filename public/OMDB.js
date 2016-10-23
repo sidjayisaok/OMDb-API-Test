ea3ba03d5e1c70a24099e2b3189b939778677b12
sidjayisaok <sidjay18@gmail.com>
2016-10-22 23:13:23 -0400

    added d3 pie graph and changed page layout

--- public/OMDB.js
+++ public/OMDB.js
@@ -1,5 +1,58 @@
 	$('#findMovie').on('click', function(){
 
+			//our d3 pie chart
+const pieGraph = (a)=>{
+    //remove duplicates
+    d3.select("svg").remove();
+    //set up pie graph
+    let w = 300,                        
+        h = 300,
+        r = 150
+
+		console.log(a);
+
+        color = d3.scale.category20c();
+        //my data fields    
+        data = [
+                {"label":"IMDB rating", "value": (10*a)/100},
+				{"label":"Unfavorables", "value": (100 - (10*a))/100 }
+               ];
+
+    //target the page with data
+    let vis = d3.select("#piegraph")
+                .append("svg:svg")             
+                .data([data])
+                .attr("width", w)           
+                .attr("height", h)
+                .attr("class", "myTarget")
+                .append("svg:g")           
+                .attr("transform", "translate(" + r + "," + r + ")")
+                 
+    let arc = d3.svg.arc()              
+                .outerRadius(r);
+    let pie = d3.layout.pie()           
+                .value((d)=> { return d.value; }); 
+    let arcs = vis.selectAll("g.slice")     
+                  .data(pie)
+                  .enter()                            
+                  .append("svg:g")                
+                  .attr("class", "slice"); 
+        
+        arcs.append("svg:path")
+            .attr("fill", (d, i)=> { return color(i); } ) 
+            .attr("d", arc);                                    
+        
+        arcs.append("svg:text")                                     
+            .attr("transform", (d)=> {                   
+                d.innerRadius = 0;
+                d.outerRadius = r;
+                return "translate(" + arc.centroid(d) + ")";
+            })
+            .attr("text-anchor", "middle")
+            .style("fill", "#335533")
+            .text((d, i)=> {return data[i].label + ": " + data[i].value.toFixed(2)*100;});
+}  
+
 		//animate.css logic for poster
 		var bounceClass ='animated zoomIn';
 		var flipClass ='animated flip';
@@ -22,15 +75,19 @@
 			method: 'GET'
 		})
 		.done(function(response) {
-			$("#movieTitle").html(response.Title);
-			$("#movieActors").html("Starring " + response.Actors);
-			$("#movieRated").html("Rated " + response.Rated);
-			$("#movieRelease").html(response.Released);
-			$("#movieRuntime").html(response.Runtime);
-			$("#movieRating").html("IMDb rates this a " + response.imdbRating);
-			$("#tomatoesRating").html("Rotten Tomatoes rates this a " + response.tomatoRating);
-			$("#movieLanguage").html("Languages include: " + response.Language);
-			$("#movieAwards").html(response.Awards);
+			$("#movieTitle").html("<h4>" + response.Title + "</h4>");
+			$("#movieActors").html("<h4>Starring " + response.Actors + "</h4>");
+			$("#movieRated").html("<h4>Rated " + response.Rated + "</h4>");
+			$("#movieRelease").html("<h4>" + response.Released + "</h4>");
+			$("#movieRuntime").html("<h4>" + response.Runtime + "</h4>");
+			$("#movieRating").html("<h4>IMDb rates this a " + response.imdbRating + "</h4>");
+			$("#tomatoesRating").html("<h4>Rotten Tomatoes rates this a " + response.tomatoRating + "</h4>");
+			$("#movieLanguage").html("<h4>Languages include: " + response.Language + "</h4>");
+			$("#movieAwards").html("<h4>" + response.Awards + "</h4>");
+
+			//piegraph attempt
+			$("#piegraph").html(pieGraph(response.imdbRating));
+
 			//grabbing pictures is a bit more complicated
 			var imageUrl = response.Poster;
 			var img = $("<img>");
@@ -100,3 +157,5 @@
   return false;
 
 	})
+
+
